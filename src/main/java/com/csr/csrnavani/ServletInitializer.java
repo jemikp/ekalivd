9895156bbb93cb725a7a21e9b6a2cafcec807df8
jay.suthar2 <jay.suthar2@eclinicalworks.com>
2024-03-23 09:56:17 -0400

    external tomcat done

--- src/main/java/com/csr/csrnavani/ServletInitializer.java
+++ src/main/java/com/csr/csrnavani/ServletInitializer.java
@@ -1,12 +1,16 @@
 package com.csr.csrnavani;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+	private static final Logger initLogger = LogManager.getLogger(ServletInitializer.class);
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		initLogger.fatal("[ APP STARTED ]");
 		return application.sources(CsrnavaniApplication.class);
 	}
 
