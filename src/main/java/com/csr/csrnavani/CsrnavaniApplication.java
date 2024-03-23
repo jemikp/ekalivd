c50910fc00ab253cdcca54997457fc678bcf2e50
jay.suthar2 <jay.suthar2@eclinicalworks.com>
2024-03-23 06:36:53 -0400

    initial commit

---
+++ src/main/java/com/csr/csrnavani/CsrnavaniApplication.java
@@ -0,0 +1,13 @@
+package com.csr.csrnavani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrnavaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrnavaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csr/csrnavani/CsrnavaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csr.csrnavani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrnavaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
