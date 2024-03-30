5690242918076bf92db94aef0f332197c5e225a6
jay.suthar2 <jay.suthar2@eclinicalworks.com>
2024-03-30 08:53:45 -0400

    Change Project Name

--- src/main/java/org/ekal/ivd/EkalIvd.java
+++ src/main/java/org/ekal/ivd/EkalIvd.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsrnavaniApplication {
+public class EkalIvd {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CsrnavaniApplication.class, args);
+		SpringApplication.run(EkalIvd.class, args);
 	}
 
 }
--- src/main/java/org/ekal/ivd/ServletInitializer.java
+++ src/main/java/org/ekal/ivd/ServletInitializer.java
@@ -10,7 +10,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		initLogger.fatal("[ APP STARTED ]");
-		return application.sources(CsrnavaniApplication.class);
+		return application.sources(EkalIvd.class);
 	}
 
 }
