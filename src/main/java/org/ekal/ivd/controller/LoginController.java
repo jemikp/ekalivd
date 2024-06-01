8ae1b582b8c30e635c7040a296a073dc94b888a8
jay.suthar2 <jay.suthar2@eclinicalworks.com>
2024-06-01 14:10:15 -0400

    Issue#33 - SessionFilter Implemented

--- src/main/java/org/ekal/ivd/controller/LoginController.java
+++ src/main/java/org/ekal/ivd/controller/LoginController.java
@@ -1,20 +1,23 @@
 package org.ekal.ivd.controller;
 
 
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
 import org.ekal.ivd.dao.UserDao;
 import org.ekal.ivd.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+@Log4j2
 @RestController
 @RequestMapping("LoginController")
 public class LoginController {
@@ -22,21 +25,32 @@ public class LoginController {
     UserDao userDao;
 
     @PostMapping("/sendOTP")
-    public ResponseEntity<?> sendOTP(@RequestParam(name = "whatsApp") String whatsApp) {
+    public ResponseEntity<?> sendOTP(@RequestParam(name = "whatsApp") String whatsApp, HttpSession session) {
         userDao.sendOtp(whatsApp);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PostMapping("/validateOTP")
-    public ResponseEntity<?> login(@RequestParam(name = "whatsApp") String whatsApp, @RequestParam(name = "otp") String otp) {
+    public ResponseEntity<?> login(@RequestParam(name = "whatsApp") String whatsApp, @RequestParam(name = "otp") String otp, HttpSession session) {
         var user = userDao.validateOTP(whatsApp, otp);
         if (user.getId() > 0) {
-            SecurityContext context =  SecurityContextHolder.getContext();
-            Authentication authentication = new UsernamePasswordAuthenticationToken(user, user.getOtp());
-            context.setAuthentication(authentication);
+            session.setAttribute("user", user);
+            session.setAttribute("userId", user.getId());
             return ResponseEntity.status(HttpStatus.OK).body(new UserDTO(user));
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid");
         }
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpSession session, HttpServletResponse response) throws IOException {
+        try {
+            session.removeAttribute("user");
+            session.removeAttribute("userId");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        session.invalidate();
+        response.sendRedirect(session.getServletContext().getContextPath() + "/public/login.html");
     }
 }
---
+++ src/main/java/org/ekal/ivd/security/SessionFilter.java
@@ -0,0 +1,85 @@
+package org.ekal.ivd.security;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+@Order(1)
+@Log4j2
+public class SessionFilter implements Filter {
+
+
+    private static final String[] SESSIONLESS_URLS = {
+            "\\/LoginController\\/.*.",
+            "\\/public\\/.*.",
+            "\\/index.html",
+            "\\/error404.html"
+    };
+    private static final ArrayList<Pattern> SESSIONLESS_PATTERNS = new ArrayList<>();
+
+    private static boolean checkIfSessionLessUrl(String uri) {
+        for (Pattern pat : SESSIONLESS_PATTERNS) {
+            Matcher matcher = pat.matcher(uri);
+            if (matcher.matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        String contextPath = filterConfig.getServletContext().getContextPath();
+        for (String urlStr : SESSIONLESS_URLS) {
+            String pattern = "\\" + contextPath + urlStr;
+            SESSIONLESS_PATTERNS.add(Pattern.compile(pattern));
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        HttpSession session = req.getSession();
+        String path = req.getContextPath();
+        String uri = req.getRequestURI();
+        String ip = req.getHeader("X-Forwarded-For");
+        ip = StringUtils.defaultIfBlank(ip, req.getRemoteAddr());
+        ThreadContext.clearAll();
+        ThreadContext.put("USERID", "0");
+        ThreadContext.put("IP", ip);
+        int userID = NumberUtils.toInt(String.valueOf(session.getAttribute("userId")), 0);
+        if (userID > 0) {
+            ThreadContext.put("USERID", String.valueOf(userID));
+            if (uri.endsWith("/public/login.html")) {
+                res.sendRedirect(path + "/ivdMaster.html");
+            } else {
+                chain.doFilter(req, res);
+            }
+        } else if (checkIfSessionLessUrl(uri)) {
+            chain.doFilter(req, res);
+        } else {
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            res.sendRedirect(path + "/public/login.html");
+        }
+    }
+}
